d8a8781599e325f65b10cd7a3969e33219863659
87170360 <87170360@qq.com>
2021-05-01 09:02:23 +0800

    .

--- main.go
+++ main.go
@@ -5,6 +5,9 @@ import (
 	"fmt"
 	"github.com/kratos/pkg/sync/errgroup"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
 )
 
 func serve(ctx context.Context, addr string) error {
@@ -16,18 +19,31 @@ func serve(ctx context.Context, addr string) error {
 
 	go func() {
 		<-ctx.Done()
-
+		err := s.Shutdown(ctx)
+		fmt.Printf("shut down serve :%v, err:%v\n", addr, err)
 	}()
 	return s.ListenAndServe()
 }
 
 func main() {
-	g := new(errgroup.Group)
+	ctx, cancel := context.WithCancel(context.Background())
+
+	sinChan := make(chan os.Signal, 1)
+	signal.Notify(sinChan, os.Interrupt, syscall.SIGTERM)
+	go func() {
+		signal := <-sinChan
+		fmt.Printf("receive signal :%v\n", signal)
+		cancel()
+	}()
+
+	g := errgroup.WithCancel(ctx)
+
 	var addrs = []string{
 		"127.0.0.1:8001",
 		"127.0.0.1:8002",
 		"127.0.0.1:8003",
 	}
+
 	for _, addr := range addrs {
 		addr := addr
 		g.Go(func(ctx context.Context) error {
@@ -38,4 +54,8 @@ func main() {
 	if err := g.Wait(); err != nil {
 		fmt.Println(err)
 	}
+
+
+	fmt.Println("main quit")
+
 }
